89d0cde9f6182d0949c257d104c3824cb1282623
Surya Kumar N K <suryakumarnk007@gmail.com>
2024-05-21 12:05:34 +0530

    Added single ton object in linked list

--- Leetcode/MergingInBetweenLinkedList/ListNode.java
+++ Leetcode/MergingInBetweenLinkedList/ListNode.java
@@ -3,7 +3,14 @@ package Leetcode.MergingInBetweenLinkedList;
 public class ListNode {
     int val;
     ListNode next;
+    private static ListNode singleTon = null;
     ListNode() {}
+    public static ListNode getInstance(int val) {
+        if (singleTon == null) {
+            singleTon = new ListNode(val);
+        }
+        return singleTon;
+    }
     ListNode(int val) { this.val = val; }
     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
 }
